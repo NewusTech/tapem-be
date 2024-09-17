f592a5bf8816a2df2a4b470166fef9aef3def7e7
raiszidane95 <raiszidane95@gmail.com>
2024-09-17 16:17:42 +0700

    remove role checking except Super Admin

--- routes/personil.route.js
+++ routes/personil.route.js
@@ -10,10 +10,10 @@ const multer = require('multer');
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-route.post('/personil/create', [mid.checkRolesAndLogout(['Kabag', 'Super Admin', 'Verifikasi', 'Admin'])], upload.single('image'), personilController.createpersonil);
+route.post('/personil/create', [mid.checkRolesAndLogout([, 'Super Admin'])], upload.single('image'), personilController.createpersonil);
 route.get('/personil/get', personilController.getpersonil); 
 route.get('/personil/get/:id', personilController.getpersonilById); 
-route.put('/personil/update/:id', [mid.checkRolesAndLogout(['Kabag', 'Super Admin', 'Verifikasi', 'Admin'])], upload.single('image'), personilController.updatepersonil); 
-route.delete('/personil/delete/:id', [mid.checkRolesAndLogout(['Kabag', 'Super Admin', 'Verifikasi', 'Admin'])], personilController.deletepersonil);
+route.put('/personil/update/:id', [mid.checkRolesAndLogout([, 'Super Admin'])], upload.single('image'), personilController.updatepersonil); 
+route.delete('/personil/delete/:id', [mid.checkRolesAndLogout([, 'Super Admin'])], personilController.deletepersonil);
 
 module.exports = route;
