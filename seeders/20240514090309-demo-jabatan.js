4f3ab48b7ade8fe13cc638e981cc2e351d852ac8
raiszidane95 <raiszidane95@gmail.com>
2024-09-17 11:16:12 +0700

    add Seeder level to Jabatan

--- seeders/20240514090309-demo-jabatan.js
+++ seeders/20240514090309-demo-jabatan.js
@@ -1,27 +1,33 @@
 'use strict';
 
+const { level } = require("winston");
+
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     const Jabatans = [
       {
         title: 'Kepala Dinas',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        level: 1
       },
       {
         title: 'Sekretaris Dinas',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        level: 2
       },
       {
         title: 'Kepala Bidang',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        level: 3
       },
       {
         title: 'Staff Khusus',
         createdAt: new Date(),
-        updatedAt: new Date()
+        updatedAt: new Date(),
+        level: 4
       },
     ];
 
