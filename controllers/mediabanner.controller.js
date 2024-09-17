97cd7cb20d35880b341dd77eaf77567872f5245c
raiszidane95 <raiszidane95@gmail.com>
2024-09-17 16:49:13 +0700

    add endpoint update

--- controllers/mediabanner.controller.js
+++ controllers/mediabanner.controller.js
@@ -1,3 +1,4 @@
+const { min } = require('moment-timezone');
 const { response } = require('../helpers/response.formatter');
 // const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const { MediaBanner } = require('../models');
@@ -35,5 +36,78 @@ module.exports = {
       response(res, 400, false, 'Get Media Banner Failed', error);
       console.log(error);
     }
-  }
+  }, 
+  updateMediaBanner: async (req, res) => {
+    try {
+      // buat schema validasi
+      const schema = {
+        title: {
+          type: "string",
+          optional: true,
+          min: 3
+        },
+        subTitle: {
+          type: "string",
+          optional: true,
+          min: 3
+        },
+        mediaLink:{
+          type: "string",
+          optional: true,
+          min: 3
+        },
+        description: {
+          type: "string",
+          optional: true,
+          min: 3
+        }
+      }
+
+      //mendapatkan data MediaBanner untuk pengecekan
+      let MediaBannerGet = await MediaBanner.findOne({
+        where: {
+          id: req.params.id
+        }
+      });
+
+      if (!MediaBannerGet) {
+        res.status(404).json(response(404, 'data not found'));
+        return;
+      }
+
+      const MediaBannerUpdateObj = {
+        title: req.body.title,
+        subTitle: req.body.subTitle,
+        mediaLink: req.body.mediaLink,
+        description: req.body.description
+      }
+
+      //validasi menggunakan module fastest-validator
+      const validate = v.validate(MediaBannerUpdateObj, schema);
+      if (validate.length > 0) {
+        res.status(400).json(response(400, 'validation failed', validate));
+        return;
+      }
+
+      //update MediaBanner
+      await MediaBanner.update(MediaBannerUpdateObj, {
+        where: {
+          id: req.params.id,
+        }
+      });
+
+      //mendapatkan data MediaBanner setelah update
+      let MediaBannerAfterUpdate = await MediaBanner.findOne({
+        where: {
+          id: req.params.id,
+        }
+      });
+
+      //response menggunakan helper response.formatter
+      res.status(200).json(response(200, 'success update Media Banner', MediaBannerAfterUpdate));
+    } catch (error) {
+      res.status(500).json(response(500, 'internal server error', error));
+      console.log(error);
+    }
+  },
 }
--- routes/mediabanner.route.js
+++ routes/mediabanner.route.js
@@ -8,7 +8,7 @@ const route = express.Router();
 // route.post('/mediabanner/create', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), regioninfo.createRegionInfo);
 route.get('/mediabanner/get', mediabanner.getMediaBanners); 
 route.get('/mediabanner/get/:id', mediabanner.getMediaBannerById); 
-// route.put('/mediabanner/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('image'), regioninfo.updateRegionInfo); 
+route.put('/mediabanner/update/:id', [mid.checkRolesAndLogout(['Super Admin'])], mediabanner.updateMediaBanner); 
 // route.delete('/mediabanner/delete/:id', [mid.checkRolesAndLogout(['Super Admin'])], regioninfo.deleteRegionInfo);
 
 module.exports = route;
