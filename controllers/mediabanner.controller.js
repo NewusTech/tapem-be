d120606a31a5c138ff1a484002ffa7271a02e92e
Aldi Nugraha <aldi.nugrahatk@gmail.com>
2024-11-04 11:20:15 +0700

    upload link atau video

--- controllers/mediabanner.controller.js
+++ controllers/mediabanner.controller.js
@@ -1,17 +1,17 @@
-const { response } = require('../helpers/response.formatter');
+const { response } = require("../helpers/response.formatter");
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
-const { MediaBanner } = require('../models');
-const logger = require('../errorHandler/logger');
+const { MediaBanner } = require("../models");
+const logger = require("../errorHandler/logger");
 const Validator = require("fastest-validator");
 const v = new Validator();
 
 const s3Client = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  useAccelerateEndpoint: true
+  useAccelerateEndpoint: true,
 });
 
 module.exports = {
@@ -20,43 +20,45 @@ module.exports = {
       // buat schema validasi
       const schema = {
         title: {
-          type: 'string',
+          type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         subTitle: {
-          type: 'string',
+          type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         mediaLink: {
-          type: 'string',
+          type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         description: {
-          type: 'string',
+          type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         typevideo: { type: "string", optional: true, convert: true },
       };
 
-      if (req.file) {
+      if (req.file && req.typevideo === 2) {
         const timestamp = new Date().getTime();
         const uniqueFileName = `${timestamp}-${req.file.originalname}`;
         const uploadParams = {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: `${process.env.PATH_AWS}/mediabanner/${uniqueFileName}`,
           Body: req.file.buffer,
-          ACL: 'public-read',
-          ContentType: req.file.mimetype
+          ACL: "public-read",
+          ContentType: req.file.mimetype,
         };
 
         const command = new PutObjectCommand(uploadParams);
 
         await s3Client.send(command);
         mediaLink = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${uploadParams.Key}`;
+      } else {
+        mediaLink = req.body.mediaLink;
       }
 
       // buat object media banner
@@ -72,19 +74,20 @@ module.exports = {
 
       const validate = v.validate(mediaBannerCreateObj, schema);
       if (validate.length > 0) {
-        res.status(400).json(response(400, 'validation failed', validate));
+        res.status(400).json(response(400, "validation failed", validate));
         return;
       }
 
       // buat media banner
       let mediaBannerCreate = await MediaBanner.create(mediaBannerCreateObj);
 
-      res.status(200).json(response(200, 'success create Media Banner', mediaBannerCreate));
-
+      res
+        .status(200)
+        .json(response(200, "success create Media Banner", mediaBannerCreate));
     } catch (error) {
       logger.error(`Error : ${error}`);
       logger.error(`Error message: ${error.message}`);
-      res.status(500).json(response(500, 'internal server error', error));
+      res.status(500).json(response(500, "internal server error", error));
       console.log(error);
     }
   },
@@ -93,11 +96,13 @@ module.exports = {
     try {
       const mediaBanner = await MediaBanner.findAll();
 
-      res.status(200).json(response(200, 'success get Media Banner', mediaBanner));
+      res
+        .status(200)
+        .json(response(200, "success get Media Banner", mediaBanner));
     } catch (error) {
       logger.error(`Error : ${error}`);
       logger.error(`Error message: ${error.message}`);
-      response(res, 400, false, 'Get Media Banner Failed', error);
+      response(res, 400, false, "Get Media Banner Failed", error);
       console.log(error);
     }
   },
@@ -106,20 +111,22 @@ module.exports = {
     try {
       const mediaBanner = await MediaBanner.findOne({
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+        },
       });
 
       if (!mediaBanner) {
-        res.status(404).json(response(404, 'data not found'));
+        res.status(404).json(response(404, "data not found"));
         return;
       }
 
-      res.status(200).json(response(200, 'success get Media Banner', mediaBanner));
+      res
+        .status(200)
+        .json(response(200, "success get Media Banner", mediaBanner));
     } catch (error) {
       logger.error(`Error : ${error}`);
       logger.error(`Error message: ${error.message}`);
-      response(res, 400, false, 'Get Media Banner Failed', error);
+      response(res, 400, false, "Get Media Banner Failed", error);
       console.log(error);
     }
   },
@@ -130,35 +137,35 @@ module.exports = {
         title: {
           type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         subTitle: {
           type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         mediaLink: {
           type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         description: {
           type: "string",
           optional: true,
-          min: 3
+          min: 3,
         },
         typevideo: { type: "string", optional: true, convert: true },
-      }
+      };
 
       //mendapatkan data MediaBanner untuk pengecekan
       let MediaBannerGet = await MediaBanner.findOne({
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+        },
       });
 
       if (!MediaBannerGet) {
-        res.status(404).json(response(404, 'data not found'));
+        res.status(404).json(response(404, "data not found"));
         return;
       }
 
@@ -169,8 +176,8 @@ module.exports = {
           Bucket: process.env.AWS_S3_BUCKET,
           Key: `${process.env.PATH_AWS}/mediabanner/${uniqueFileName}`,
           Body: req.file.buffer,
-          ACL: 'public-read',
-          ContentType: req.file.mimetype
+          ACL: "public-read",
+          ContentType: req.file.mimetype,
         };
 
         const command = new PutObjectCommand(uploadParams);
@@ -185,12 +192,12 @@ module.exports = {
         mediaLink: req.file ? mediaLink : MediaBannerGet.mediaLink,
         description: req.body.description,
         typevideo: req.body.typevideo,
-      }
+      };
 
       //validasi menggunakan module fastest-validator
       const validate = v.validate(MediaBannerUpdateObj, schema);
       if (validate.length > 0) {
-        res.status(400).json(response(400, 'validation failed', validate));
+        res.status(400).json(response(400, "validation failed", validate));
         return;
       }
 
@@ -198,22 +205,26 @@ module.exports = {
       await MediaBanner.update(MediaBannerUpdateObj, {
         where: {
           id: req.params.id,
-        }
+        },
       });
 
       //mendapatkan data MediaBanner setelah update
       let MediaBannerAfterUpdate = await MediaBanner.findOne({
         where: {
           id: req.params.id,
-        }
+        },
       });
 
       //response menggunakan helper response.formatter
-      res.status(200).json(response(200, 'success update Media Banner', MediaBannerAfterUpdate));
+      res
+        .status(200)
+        .json(
+          response(200, "success update Media Banner", MediaBannerAfterUpdate)
+        );
     } catch (error) {
       logger.error(`Error : ${error}`);
       logger.error(`Error message: ${error.message}`);
-      res.status(500).json(response(500, 'internal server error', error));
+      res.status(500).json(response(500, "internal server error", error));
       console.log(error);
     }
   },
@@ -223,12 +234,12 @@ module.exports = {
       //mendapatkan data MediaBanner untuk pengecekan
       let MediaBannerGet = await MediaBanner.findOne({
         where: {
-          id: req.params.id
-        }
+          id: req.params.id,
+        },
       });
 
       if (!MediaBannerGet) {
-        res.status(404).json(response(404, 'data not found'));
+        res.status(404).json(response(404, "data not found"));
         return;
       }
 
@@ -236,16 +247,15 @@ module.exports = {
       await MediaBanner.destroy({
         where: {
           id: req.params.id,
-        }
+        },
       });
 
-      res.status(200).json(response(200, 'success delete Media Banner'));
-
+      res.status(200).json(response(200, "success delete Media Banner"));
     } catch (error) {
       logger.error(`Error : ${error}`);
       logger.error(`Error message: ${error.message}`);
-      res.status(500).json(response(500, 'internal server error', error));
+      res.status(500).json(response(500, "internal server error", error));
       console.log(error);
     }
-  }
-}
+  },
+};
